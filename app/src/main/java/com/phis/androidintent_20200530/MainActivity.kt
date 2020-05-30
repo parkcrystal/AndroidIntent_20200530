b89c834a244879069dd3831e004f8a44adcc6738
parkcrystal <9jeworld@naver.com>
2020-05-30 11:03:32 +0900

    문자보내기 테스트
    => putExtra("sms_body", "내용") 으로 내용을 미리 적어둘 수 있다.

--- app/src/main/java/com/phis/androidintent_20200530/MainActivity.kt
+++ app/src/main/java/com/phis/androidintent_20200530/MainActivity.kt
@@ -36,5 +36,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+        smsBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+            val myUri = Uri.parse("smsto:${inputPhoneNum}")
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+            myIntent.putExtra("sms_body", "[광고] 이 앱을 공유헤주세요.")
+            startActivity(myIntent)
+        }
+
+
     }
 }
