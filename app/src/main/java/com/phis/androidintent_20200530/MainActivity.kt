9a8c2a039032b3564aa98f3b65cba742c652ea33
parkcrystal <9jeworld@naver.com>
2020-05-30 10:24:40 +0900

    다이얼 액션

---
+++ app/src/main/java/com/phis/androidintent_20200530/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.phis.androidintent_20200530
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        dialBtn.setOnClickListener {
+
+            val myUri = Uri.parse("tel:01012345678")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
+    }
+}
